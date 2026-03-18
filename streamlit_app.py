7d712220671520b27c33f0e4eb7378edf2ca414a
101010 <35881415+fifonc@users.noreply.github.com>
2026-03-18 13:20:08 -0400

    Enhance smoothie app with nutrition info retrieval
    Updated ingredients handling and added nutrition information display for selected fruits.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app.
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -27,8 +28,14 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string = ''.join(ingredients_list)
+    ingredients_string = ''
 
+    for fruit_chosen in ingredient_list:
+        ingredients_string += fruit_chosen + ''
+        st.subheader(fruit_chosen + ' Nutrition Information ")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+  
     if st.button("Submit Order"):
         session.sql(
             "INSERT INTO orders (ingredients, name_on_order) VALUES (?, ?)",
@@ -37,8 +44,5 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered!', icon="✅")
 
-# New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
+
